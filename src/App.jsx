09873bdb778b111304ed5141d4051abf8421e668
p00jitha <dasaripujitha9@gmail.com>
2024-05-18 20:50:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
